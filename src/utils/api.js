3e074ce281b0ed3963077ed607660dd04369ea97
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-04-11 22:35:01 +0530

    -

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://my-travel-app-backend-6.onrender.com/api',
   withCredentials: true,
 });
 

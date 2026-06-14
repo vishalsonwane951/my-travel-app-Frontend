b0c71908e41bb44ce6714fa5020f38c02a26858c
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-06-14 09:14:48 +0530

    handover

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://my-travel-app-backend-6.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
   withCredentials: true,
 });
 
